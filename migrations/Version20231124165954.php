9d8db349833a9164c177990b5ae80b38e04135be
jeremy Dubrulle <https://github.com/jeremy93130>
2023-11-24 18:18:04 +0100

    css

--- migrations/Version20231124165954.php
+++ migrations/Version20231124165954.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231124124749 extends AbstractMigration
+final class Version20231124165954 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,13 +21,17 @@ final class Version20231124124749 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE animaux CHANGE espece espece ENUM(\'chat\', \'chien\', \'oiseau\',\'rongeur\',\'reptile\'), CHANGE statut statut ENUM(\'disponible\', \'en attente\', \'adopté\'), CHANGE sexe sexe ENUM(\'Male\', \'Femelle\')');
-        $this->addSql('ALTER TABLE demandes_adoptions CHANGE animal_id_id animal_id_id INT DEFAULT NULL, CHANGE statut statut ENUM(\'En attente\', \'Acceptée\', \'Refusée\')');
+        $this->addSql('ALTER TABLE demandes_adoptions ADD animal_id_id INT DEFAULT NULL, CHANGE statut statut ENUM(\'En attente\', \'Acceptée\', \'Refusée\')');
+        $this->addSql('ALTER TABLE demandes_adoptions ADD CONSTRAINT FK_833FBC5D5EB747A3 FOREIGN KEY (animal_id_id) REFERENCES animaux (id)');
+        $this->addSql('CREATE INDEX IDX_833FBC5D5EB747A3 ON demandes_adoptions (animal_id_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE animaux CHANGE sexe sexe VARCHAR(255) DEFAULT NULL, CHANGE espece espece VARCHAR(255) DEFAULT NULL, CHANGE statut statut VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE demandes_adoptions CHANGE animal_id_id animal_id_id INT NOT NULL, CHANGE statut statut VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE demandes_adoptions DROP FOREIGN KEY FK_833FBC5D5EB747A3');
+        $this->addSql('DROP INDEX IDX_833FBC5D5EB747A3 ON demandes_adoptions');
+        $this->addSql('ALTER TABLE demandes_adoptions DROP animal_id_id, CHANGE statut statut VARCHAR(255) DEFAULT NULL');
     }
 }
